6f4d311069afe77a744f2446d4c8ffe1fe4b5306
willingWill17 <141435268+willingWill17@users.noreply.github.com>
2023-09-16 14:19:31 +0700

    dir.py

--- dir.py
+++ dir.py
@@ -1,10 +1,11 @@
 import os
-IMAGE_KEYFRAME_PATH = "/content/drive/MyDrive/HCMAI22_MiniBatch1/Keyframes/"
-VISUAL_FEATURES_PATH = "/content/drive/MyDrive/HCMAI22_MiniBatch1/CLIP_features"
+IMAGE_KEYFRAME_PATH = r"/challenge_data"
+VISUAL_FEATURES_PATH = r"/challenge_data"
 
-for folder_path2 in os.listdir(IMAGE_KEYFRAME_PATH):
-  folder_path2 = os.path.join(IMAGE_KEYFRAME_PATH, folder_path2)  
-  for folder_path in os.listdir(folder_path2):
-    folder_path = os.path.join(folder_path2, folder_path)
-    for img_paths in os.listdir(folder_path):
-      print(img_paths)
+for folder_path3 in os.listdir(IMAGE_KEYFRAME_PATH):
+  folder_path3 = os.path.join(IMAGE_KEYFRAME_PATH, folder_path3)
+  feature_path2 = os.path.join(VISUAL_FEATURES_PATH, folder_path3)
+  feature_path = os.path.join(feature_path2, 'clip-features')
+  folder_path2 = os.path.join(folder_path3, 'keyframes')
+  for img_paths in os.listdir(folder_path2):
+    img_paths = os.path.join(folder_path2, img_paths)
