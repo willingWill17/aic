476c60be2257037e82ef6a0b5aee4883b5cb97a1
willingWill17 <141435268+willingWill17@users.noreply.github.com>
2023-09-18 17:38:16 +0700

    dir.py

--- dir.py
+++ dir.py
@@ -6,9 +6,9 @@ VISUAL_FEATURES_PATH = r"/aic/challenge_data"
 for mapp in os.listdir(IMAGE_KEYFRAME_PATH):
   if mapp == 'keyframes':
     keyframes_map = os.path.join(IMAGE_KEYFRAME_PATH, mapp)
-    print(keyframes_map)
     for keyframes_dir in os.listdir(keyframes_map):
       img_paths = os.path.join(keyframes_map, keyframes_dir)
+      print(img_paths)
   elif mapp == 'clip-features':
     feature_paths = os.path.join(IMAGE_KEYFRAME_PATH, mapp)
 
