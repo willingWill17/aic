3760d791a15fdd29f614f64f30dd6577f59843db
willingWill17 <141435268+willingWill17@users.noreply.github.com>
2023-09-18 17:16:47 +0700

    dir.py

--- dir.py
+++ dir.py
@@ -2,12 +2,11 @@ import os
 IMAGE_KEYFRAME_PATH = r"/aic/challenge_data"
 VISUAL_FEATURES_PATH = r"/aic/challenge_data"
 
-for folder_path in os.listdir(IMAGE_KEYFRAME_PATH):
-  folder_map = os.path.join(IMAGE_KEYFRAME_PATH,folder_path)
-  for mapp in os.listdir(folder_map):
-    if mapp == 'keyframes':
-      keyframes_map = os.path.join(folder_map, mapp)
-      for keyframes_dir in os.listdir(keyframes_map):
-        img_paths = os.path.join(keyframes_map, keyframes_dir)
-    elif mapp == 'clip-features':
-      feature_paths = os.path.join(folder_map, mapp)
+
+for mapp in os.listdir(IMAGE_KEYFRAME_PATH):
+  if mapp == 'keyframes':
+    keyframes_map = os.path.join(IMAGE_KEYFRAME_PATH, mapp)
+    for keyframes_dir in os.listdir(keyframes_map):
+      img_paths = os.path.join(keyframes_map, keyframes_dir)
+  elif mapp == 'clip-features':
+    feature_paths = os.path.join(IMAGE_KEYFRAME_PATH, mapp)
